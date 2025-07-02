6b94be7c140284accc38e8fdc96d4b3d450bfa3e
linventif <gregoire.becue@proton.me>
2025-07-03 00:08:05 +0200

    refactor: remove obsolete loader script

--- crates/loader/load.sh
+++
@@ -1,2 +0,0 @@
-cargo build --release --target i686-unknown-linux-gnu
-mv ../../target/i686-unknown-linux-gnu/release/libgmod_integration_loader.so ../../target/i686-unknown-linux-gnu/release/gmsv_gmod_integration_
